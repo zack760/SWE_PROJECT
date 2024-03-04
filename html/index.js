a8fd8c287b62228a849e9e59ae4bf89c20a78f40
zack760 <145677207+zack760@users.noreply.github.com>
2024-03-04 14:03:54 +0000

    setting up mvp

--- html/index.js
+++ html/index.js
@@ -1,10 +1,14 @@
+let map;
+let markers = []
+
 function initMap() {
     var dublin = {lat: 53.349805, lng: -6.26031};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: dublin
     });
 }
+
 
 
 
@@ -22,8 +26,12 @@ fetch("data.json")
     return response.json();
   })
 
-  .then((data) => {stations = data; StationPing()
-    })
+  .then((data) => {
+    stations = data; 
+    initMap(); 
+    StationPing();
+  })
+
   .catch((error) => console.error("Network error: ", error));
 
 function StationPing() {
@@ -36,6 +44,65 @@ function StationPing() {
             map: map,
             title: station.name,
             station_number: station.number,
+            icon: {
+              url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+              scaledSize: new google.maps.Size(30,30),
+            },
         });
+      
+        markers.push(marker);
+        marker.addListener("click", function() {
+          var infoWindow = new google.maps.InfoWindow({
+            content: "Station Name: " + station.name + '<br>' + "Station Number: " + station.number
+          });
+          infoWindow.open(map, marker);
+        });
+
     });
+}
+
+function findRoute() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var polyline = new google.maps.Polyline({
+    path: [],
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  })
+
+  directionsDisplay.setMap(map); 
+
+  removeMarkers();
+
+  var request = {
+    origin: start,
+    destination: end, 
+    // travelMode: "BICYCLING"
+    travelMode: "DRIVING"
+  };
+
+  directionsService.route(request, function(result, status) {
+    if (status == "OK") {
+      directionsDisplay.setDirections(result);
+      var route = result.routes[0].overview_path;
+      for (var i = 0; i < route.length; i++) {
+        polyline.getPath().push(route[i]);
+      }
+      polyline.setMap(map);
+  
+    } else {
+      console.error("Directions request failed due to " + status);
+    }
+  });
+}
+
+function removeMarkers() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
 }
